938d8d4df638aba790c70790e63c5969733bbb06
ssonsk92 <101576095+ssonsk92@users.noreply.github.com>
2022-09-24 21:59:41 +0900

    프로그래머스 > 정렬 > 가장 큰 수

--- sort_max_number.cpp
+++ sort_max_number.cpp
@@ -5,49 +5,16 @@
 
 using namespace std;
 
-vector<int> get_nums(int num)
-{
-    vector<int> result;
-    if (0 == num)
-    {
-        result.emplace_back(0);
-        return result;
-    }
-
-    while (0 < num)
-    {
-        int n = num % 10;
-        num /= 10;
-        result.emplace_back(n);
-    }
-
-    reverse(result.begin(), result.end());
-
-    return result;
-}
-
 class Comp
 {
 public:
     Comp() {};
     bool operator()(int lhs, int rhs)
     {
-        vector<int> left = get_nums(lhs);
-        vector<int> right = get_nums(rhs);
+        string l = to_string(lhs);
+        string r = to_string(rhs);
 
-        size_t left_idx = 0, right_idx = 0;
-        while (left[left_idx] == right[right_idx])
-        {
-            if (left_idx == left.size() - 1 || right_idx == right.size() - 1)
-            {
-                return left[left_idx] < right[right_idx];
-            }
-
-            left_idx = min(++left_idx, left.size() - 1);
-            right_idx = min(++right_idx, right.size() - 1);
-        }
-
-        return left[left_idx] > right[right_idx];
+        return stoi(l + r) > stoi(r + l);
     }
 };
 
@@ -71,7 +38,7 @@ string solution(vector<int> numbers) {
 int main()
 {
     vector<int> numbers = { 3, 30, 34, 5, 9 };
-    //vector<int> numbers = { 979, 97, 978, 81, 818, 817 };
+    //vector<int> numbers = { 979, 97, 978, 81, 818, 817 }; //9799797881881817
     //vector<int> numbers = {97, 978 };
     //vector<int> numbers = {42, 423, 34, 3, 30 };
     //vector<int> numbers = { 3, 33, 330 };
