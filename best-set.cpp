00526b9deb67246ffc22ff4f23c730ca69d8e80f
ssonsk92 <101576095+ssonsk92@users.noreply.github.com>
2022-10-05 23:14:40 +0900

    프로그래머스 > 연습문제 > 최고의 집합 (풀이보고품)

--- best-set.cpp
+++ best-set.cpp
@@ -1,62 +1,29 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <numeric>
 
 using namespace std;
 
-void get_set(int n, int s, vector<int> v, int& multiple, vector<int>& answer)
-{
-    if (1 == n)
-    {
-        v.emplace_back(s);
-        //for (int i : v)
-        //    cout << i << ", ";
-        //cout << endl;
-
-        int m = accumulate(v.begin(), v.end(), 1, multiplies<int>());
-        if (multiple < m)
-        {
-            answer = v;
-            multiple = m;
-        }
-
-        return;
-    }
-
-    for (int i = v[v.size() - 1]; i < s - n; ++i)
-    {
-        vector<int> temp = v;
-        temp.emplace_back(i);
-        get_set(n - 1, s - i, temp, multiple, answer);
-    }
-}
-
 vector<int> solution(int n, int s) {
-    vector<int> answer;
-
     if (s < n)
         return { -1 };
 
-    int multiple = 1;
-    for (int i = 1; i < s - n; ++i)
-    {
-        vector<int> v;
-        v.emplace_back(i);
-        get_set(n - 1, s - i, v, multiple, answer);
-    }
+    vector<int> answer(n);
 
+    int t = s / n;
+    int sum = s - (t * n);
+    for (int i = 0; i < n; i++) {
+        if (i >= n - sum)
+            answer[i] = t + 1;
+        else
+            answer[i] = t;
+    }
     return answer;
 }
 
 int main()
 {
-    vector<int> v = solution(2, 9);
-
-    cout << "result : ";
-    for (int i : v)
-        cout << i << ", ";
-    cout << endl;
+    vector<int> v = solution(4, 9);
 
     return 0;
 }
